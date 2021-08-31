cbc00f5d87f7b6e449cecae7ddb4d3346c63d726
riemarnia <88694878+riemarnia@users.noreply.github.com>
2021-08-31 12:04:02 +1000

    Update aquarium.js

--- aquarium.js
+++ aquarium.js
@@ -12,9 +12,16 @@ Also store the tdCost id element as a variable.
 The following link will help you to do this:
 https://www.w3schools.com/jsref/met_document_getelementbyid.asp
 */
+function my Function() {
+		document.getElementById("demo").innerHTML = "Hello World";
+}
 
-
-
+<script>
+function myFunction() {
+	var x = document.getElementById("demo");
+	x.style.color = "red";
+}
+</script>
 
 /*
 select the "Calculate Cost" and "Reset" buttons by
@@ -26,7 +33,15 @@ The links below will help you to do this:
 https://www.w3schools.com/js/js_htmldom_document.asp
 https://www.w3schools.com/jsref/event_onclick.asp
 */
+<button onclick="myFunction()">Click me</button>
 
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+</script>
 
 
 /*
@@ -36,6 +51,13 @@ Get the value of each variable you created at the beginning
 and store each value as a new variable.
 https://www.javascript-coder.com/javascript-form/getelementbyid-form/
 
+ 
+
+
+
+
+
+
 Test whether the user has entered a number in
 the text boxes and return an alert if non-numbers are entered. If
 numbers have been entered into the text boxes, calculate the surface area
@@ -43,7 +65,14 @@ and return the result. You can use the following links to help you with this.
 https://www.w3schools.com/js/js_functions.asp
 https://www.w3schools.com/jsref/jsref_isNaN.asp
 */
+<p id="surface area"></p>
 
+<script>
+  function myFunction(p1, p2) {
+    return p1 * p2; 
+}
+document.getElementById("surface area").innerHTML = myFunction(p1 * p2);
+</script>
 
 
 /*
@@ -51,6 +80,17 @@ Create a function that calculates the length of the edges
 and returns the result.
 You can use the following link to help you with this.
 https://www.w3schools.com/js/js_functions.asp
+/*
+
+<p id="length of edges"></p>
+
+<script>
+function myFunctions(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("surface area").innerHTML = myFunction(0.10 * p2);
+
+
 
 At the beginning of your function get the value of each variable you
 created at the beginning of your program and store each value as a new variable.
@@ -82,4 +122,5 @@ Use this link to help https://www.w3schools.com/js/tryit.asp?filename=tryjs_form
 Also reset the output (tdCost). Use this link to help you do this
 https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_innerhtml_delete
 */
+
 
